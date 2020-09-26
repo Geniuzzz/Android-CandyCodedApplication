a0cf7fa93c1f0bbf1ab7fb6863669c23e456821c
victory.sam <victory.sam@globalaccelerex.com>
2020-09-26 06:08:53 +0100

    start the phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -17,7 +17,7 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
@@ -25,14 +25,16 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    public void createMapIntent(View view){
+    public void createMapIntent(View view) {
         Uri locationUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) startActivity(mapIntent);
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(dialIntent);
+    }
 }
